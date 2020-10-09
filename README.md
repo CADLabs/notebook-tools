56a170b024a5a2eb70604e912a702159e62aa641
Benjamin Scholtz <ben@bitsofether.com>
2020-10-09 22:33:07 +0200

    Add args

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # notebook-tools
 Rust CLI tools for manipulation of Jupyter Notebooks.
+
+```bash
+notebook-tools notebook.ipynb --filter background-color --clear_code --clear_output > notebook-processed.ipynb 
+```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 use std::error::Error;
@@ -10,11 +10,18 @@ use clap::clap_app;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Cell {
-    cell_type: String,
+    cell_type: CellType,
     metadata: Option<Value>,
     source: Vec<String>,
     execution_count: Option<u32>,
-    outputs: Option<Vec<Value>>
+    outputs: Option<Vec<Value>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum CellType {
+    code,
+    markdown,
+    raw,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -22,7 +29,7 @@ struct Notebook {
     cells: Vec<Cell>,
     metadata: Value,
     nbformat: u32,
-    nbformat_minor: u32
+    nbformat_minor: u32,
 }
 
 fn read_notebook_from_file<P: AsRef<Path>>(path: P) -> Result<Notebook, Box<dyn Error>> {
@@ -34,8 +41,8 @@ fn read_notebook_from_file<P: AsRef<Path>>(path: P) -> Result<Notebook, Box<dyn 
     Ok(n)
 }
 
-fn filter(cell: &Cell) -> bool {
-    !cell.source.contains(&String::from("<div style=\"outline: 2px dotted blue;\">\n"))
+fn tag_in_source(source: &Vec<String>, tag: &String) -> bool {
+    source.iter().any(|line| line.contains(tag))
 }
 
 fn main() {
@@ -44,14 +51,59 @@ fn main() {
         (author: "Ben Scholtz")
         (about: "Jupyter Notebook tools")
         (@arg input: +required "Sets the input file")
-    ).get_matches();
+        (@arg filter_cells: -f --filter +takes_value "Filter cells by source")
+        (@arg clear_code: -c --clear_code "Clear code cell source")
+        (@arg clear_output: -o --clear_output "Clear code cell output")
+    )
+    .get_matches();
 
     let notebook_source = matches.value_of("input").unwrap();
     let notebook: Notebook = read_notebook_from_file(notebook_source).unwrap();
 
-    let cells: Vec<Cell> = notebook.cells
+    let cells: Vec<Cell> = notebook
+        .cells
         .into_iter()
-        .filter(|cell| filter(cell))
+        .filter(|cell| {
+            if matches.is_present("filter_cells") {
+                let tag: String = matches.value_of("filter_cells").unwrap().into();
+                // &String::from("background-color")
+                match cell.cell_type {
+                    CellType::code => !tag_in_source(&cell.source, &tag),
+                    CellType::markdown => !tag_in_source(&cell.source, &tag),
+                    CellType::raw => !tag_in_source(&cell.source, &tag),
+                }
+            } else {
+                true
+            }
+        })
+        .map(|cell| {
+            if matches.is_present("clear_code") {
+                match cell.cell_type {
+                    CellType::code => Cell {
+                        source: Vec::new(),
+                        ..cell
+                    },
+                    CellType::markdown => cell,
+                    CellType::raw => cell,
+                }
+            } else {
+                cell
+            }
+        })
+        .map(|cell| {
+            if matches.is_present("clear_output") {
+                match cell.cell_type {
+                    CellType::code => Cell {
+                        outputs: Some(Vec::new()),
+                        ..cell
+                    },
+                    CellType::markdown => cell,
+                    CellType::raw => cell,
+                }
+            } else {
+                cell
+            }
+        })
         .collect();
 
     let result = Notebook {
